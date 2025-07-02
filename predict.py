e77b261b6842c09ce068ad55108c0dddf002422c
Daffa Aditya <79897029+daffadevhosting@users.noreply.github.com>
2025-07-02 12:27:05 +0700

    penyesuaian predict.py

--- predict.py
+++ predict.py
@@ -1,14 +1,16 @@
+from cog import BasePredictor, Input, Path
 from diffusers import StableDiffusionPipeline
 import torch
 
-class Predictor:
+class Predictor(BasePredictor):
     def setup(self):
         self.pipe = StableDiffusionPipeline.from_pretrained(
             "runwayml/stable-diffusion-v1-5",
             torch_dtype=torch.float16
         ).to("cuda")
 
-    def predict(self, prompt: str) -> str:
+    def predict(self, prompt: str = Input(description="Prompt")) -> Path:
         image = self.pipe(prompt).images[0]
-        image.save("output.png")
-        return "output.png"
+        output_path = "/tmp/output.png"
+        image.save(output_path)
+        return Path(output_path)
